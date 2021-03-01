069a68d49e8c47a671e3aeb9ad72975275f1873a
Felix <felixbinder123@gmx.de>
2021-03-01 09:42:30 +0100

    Initial commit

---
+++ src/main/java/de/derbinder/partzservice/PartzServiceApplication.java
@@ -0,0 +1,13 @@
+package de.derbinder.partzservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartzServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartzServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/derbinder/partzservice/PartzServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.derbinder.partzservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartzServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
